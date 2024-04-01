f6461330264c7cb44421fddaa6b0c48fdfcff272
bpoole16 <bpoole604@gmail.com>
2024-04-01 13:46:32 -0400

    Update version for setup file.

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='ai_safety_gridworlds', 
-    version='1.0', 
+    version='1.5', 
     packages=find_packages(include=['ai_safety_gridworlds'])
 )
